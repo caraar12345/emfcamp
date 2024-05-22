d56e581f3bea3edd9d97fb026254d5f3d7b6ebbe
Aaron Carson <aaron@aaroncarson.co.uk>
2024-05-22 09:48:11 +0000

    Change URL to 2024

--- schedule-to-ics/const.go
+++ schedule-to-ics/const.go
@@ -1,5 +1,5 @@
 package main
 
-const scheduleUrl = "https://www.emfcamp.org/schedule/2022.json"
+const scheduleUrl = "https://www.emfcamp.org/schedule/2024.json"
 const fullTimeLayout = "2006-01-02 15:04:05"
 const timeLayout = "15:04"
